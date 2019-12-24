a80b3dfcd2ede0c1aaf2f9259cef2df59ff1c6ee
NotnaKO <kopanovanton20062004>
2019-12-24 21:18:56 +0300

    Немного начал делать игровые классы, однако без игрового процесса это оказалось довольно сложно

--- game.py
+++ game.py
@@ -1,7 +1,4 @@
-import pygame
-import sys
-import os
-from start import *
+from lessons import *
 
 start_screen()
 player_group = pygame.sprite.Group()
@@ -11,3 +8,23 @@ images = {'player': load_image('player.jpg')}
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(player_group)
+        self.image = load_image('player.png')
+        self.rect = self.image.get_rect()
+        self.play = True
+        self.damage = 10
+        self.health = 100
+        self.speed = 10
+
+    def hurt(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.play = False
+
+    def heal(self, health):
+        self.health += health
+        if self.health > 100:
+            self.health = 100
+
+    def move(self):
+        self.rect.y -= self.speed
+
