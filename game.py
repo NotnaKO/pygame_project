75df2c91740c85051ee9445aba1f7c307a617b94
NotnaKO <kopanovanton20062004>
2019-12-25 11:50:14 +0300

    Движение игрока сделано

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from lessons import *
+import pygame
 
 player_group = pygame.sprite.Group()
 meteors_group = pygame.sprite.Group()
@@ -15,7 +16,7 @@ class Player(pygame.sprite.Sprite):
         self.play = True
         self.damage = 10
         self.health = 100
-        self.speed = 10
+        self.speed = PLAYER_SPEED
 
     def hurt(self, damage):
         self.health -= damage
@@ -28,7 +29,37 @@ class Player(pygame.sprite.Sprite):
             self.health = 100
 
     def move(self):
-        self.rect.y -= self.speed
+        if self.play:
+            self.rect.y -= self.speed
+
+    def update(self):  # делает стандартный ход
+        self.move()
+        if pygame.sprite.spritecollideany(self, meteors_group):
+            spr = pygame.sprite.spritecollideany(self, meteors_group)
+            spr.hurt(self.damage)
+            self.hurt(self.damage)
+        if righting and not lefting:
+            self.move_right()
+        if lefting and not righting:
+            self.move_left()
+        if accel and not deccel:
+            self.acceleration()
+        if deccel and not accel:
+            self.deceleration()
+
+    def move_right(self):
+        if self.rect.right + PLAYER_SPEED <= WIDTH:
+            self.rect.x += PLAYER_SPEED
+
+    def move_left(self):
+        if self.rect.x - PLAYER_SPEED >= 0:
+            self.rect.x -= PLAYER_SPEED
+
+    def acceleration(self):  # ускорение
+        self.rect.y -= PLAYER_SPEED // 2
+
+    def deceleration(self):
+        self.rect.y += PLAYER_SPEED // 2
 
 
 class Meteor(pygame.sprite.Sprite):
@@ -42,10 +73,14 @@ class Meteor(pygame.sprite.Sprite):
         self.damage = 50
         self.health = 30
 
-
     def move(self):
         self.rect.x += self.vect[0]
         self.rect.y += self.vect[1]
+
+    def hurt(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            pass  # Здесь должна быть анимация уничтожения метеора
 
 
 class Camera:
@@ -62,9 +97,10 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h - HEIGHT)
 
 
-GAME_SPEED = 200
-COLCOUNT = WIDTH // 9 # Адаптировать к разным уровням
+GAME_SPEED = 200  # дальность расположения метеоров
+COLCOUNT = WIDTH // 9  # Адаптировать к разным уровням
 MYEVENTTYPE = 10
+PLAYER_SPEED = 2
 start_screen()
 levelmap = display_lessons()
 camera = Camera()
@@ -81,6 +117,7 @@ for i in range(lw):
         sp.append('-')
     else:
         sp.append('P')
+levelmap.append(''.join(sp).replace('P', '-'))
 levelmap.append(''.join(sp))
 
 
@@ -99,14 +136,38 @@ def view_lesson():
 player = view_lesson()
 fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
 screen.blit(fon, (0, 0))
+righting, lefting = False, False
+accel, deccel = False, False
 
 while True:
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                lefting = True
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                righting = True
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                accel = True
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                deccel = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                lefting = False
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                righting = False
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                accel = False
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                deccel = False
+    player.update()
     camera.update(player)
     for sprite in all_sprites:
         camera.apply(sprite)
     all_sprites.draw(screen)
+
     pygame.display.flip()
     clock.tick(FPS)
